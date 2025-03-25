ffd7ecaea124d7e11e27625a0589bd33698baecb
Huxulm <huxulm@gmail.com>
2025-03-25 11:51:20 +0800

    docs: improve documentation formatting in obl80.rs

--- src/pnp/obl80.rs
+++ src/pnp/obl80.rs
@@ -8,25 +8,27 @@ use crate::consts::{DAS2R, DJ00, DJC};
 ///  Status:  canonical model.
 ///
 ///  Given:
+///  ```
 ///     date1,date2   double    TT as a 2-part Julian Date (Note 1)
-///
+///  ```
 ///  Returned (function value):
+///  ```
 ///                   double    obliquity of the ecliptic (radians, Note 2)
-///
+///  ```
 ///  Notes:
 ///
 ///  1) The TT date date1+date2 is a Julian Date, apportioned in any
 ///     convenient way between the two arguments.  For example,
 ///     JD(TT)=2450123.7 could be expressed in any of these ways,
 ///     among others:
-///
+///  ```
 ///            date1          date2
 ///
 ///         2450123.7           0.0       (JD method)
 ///         2451545.0       -1421.3       (J2000 method)
 ///         2400000.5       50123.2       (MJD method)
 ///         2450123.5           0.2       (date & time method)
-///
+///  ```
 ///     The JD method is the most natural and convenient to use in
 ///     cases where the loss of several decimal digits of resolution
 ///     is acceptable.  The J2000 method is best matched to the way
