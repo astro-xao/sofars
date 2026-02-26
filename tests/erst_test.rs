7faa6061638a2fbc3469f1c932d399f94aedfac8
Huxulm <huxulm@gmail.com>
2026-02-26 18:34:05 +0800

    test(erst): refactor unit tests to use vvd assertion helper for accuracy verification

--- tests/erst_test.rs
+++ tests/erst_test.rs
@@ -1,123 +1,126 @@
-use sofars::erst::{
-    ee00, ee00a, ee00b, ee06a, eect00, eqeq94, gmst00, gmst06, gmst82, gst00a, gst00b, gst06,
-    gst06a, gst94,
-};
+mod common;
 
-#[test]
-fn test_ee00() {
-    let epsa = 0.4090789763356509900;
-    let dpsi = -0.9630909107115582393e-5;
+#[cfg(test)]
+mod tests {
+    use super::common::vvd;
+    use sofars::erst;
 
-    let ee = ee00(2400000.5, 53736.0, epsa, dpsi);
+    #[test]
+    fn test_ee00() {
+        let epsa = 0.4090789763356509900;
+        let dpsi = -0.9630909107115582393e-5;
 
-    assert!((ee - (-0.8834193235367965479e-5)).abs() < 1e-18, "ee00");
-}
+        let ee = erst::ee00(2400000.5, 53736.0, epsa, dpsi);
 
-#[test]
-fn test_ee00a() {
-    let ee = ee00a(2400000.5, 53736.0);
+        vvd(ee, -0.8834193235367965479e-5, 1e-18, "ee00", "status");
+    }
 
-    assert!((ee - (-0.8834192459222588227e-5)).abs() < 1e-18, "ee00a");
-}
+    #[test]
+    fn test_ee00a() {
+        let ee = erst::ee00a(2400000.5, 53736.0);
 
-#[test]
-fn test_ee00b() {
-    let ee = ee00b(2400000.5, 53736.0);
+        vvd(ee, -0.8834192459222588227e-5, 1e-18, "ee00a", "status");
+    }
 
-    assert!((ee - (-0.8835700060003032831e-5)).abs() < 1e-18, "ee00b");
-}
+    #[test]
+    fn test_ee00b() {
+        let ee = erst::ee00b(2400000.5, 53736.0);
 
-#[test]
-fn test_ee06a() {
-    let ee = ee06a(2400000.5, 53736.0);
+        vvd(ee, -0.8835700060003032831e-5, 1e-18, "ee00b", "status");
+    }
 
-    assert!((ee - (-0.8834195072043790156e-5)).abs() < 1e-15, "ee06a");
-}
+    #[test]
+    fn test_ee06a() {
+        let ee = erst::ee06a(2400000.5, 53736.0);
 
-#[test]
-fn test_eect00() {
-    let eect = eect00(2400000.5, 53736.0);
+        vvd(ee, -0.8834195072043790156e-5, 1e-15, "ee06a", "status");
+    }
 
-    assert!((eect - 0.2046085004885125264e-8).abs() < 1e-20, "eect00");
-}
+    #[test]
+    fn test_eect00() {
+        let eect = erst::eect00(2400000.5, 53736.0);
 
-#[test]
-fn test_eqeq94() {
-    let eqeq = eqeq94(2400000.5, 41234.0);
+        vvd(eect, 0.2046085004885125264e-8, 1e-20, "eect00", "status");
+    }
 
-    assert!((eqeq - 0.5357758254609256894e-4).abs() < 1e-17, "eqeq94");
-}
+    #[test]
+    fn test_eqeq94() {
+        let eqeq = erst::eqeq94(2400000.5, 41234.0);
 
-#[test]
-fn test_gmst00() {
-    let theta = gmst00(2400000.5, 53736.0, 2400000.5, 53736.0);
+        vvd(eqeq, 0.5357758254609256894e-4, 1e-17, "eqeq94", "status");
+    }
 
-    assert!((theta - 1.754174972210740592).abs() < 1e-12, "gmst00");
-}
+    #[test]
+    fn test_gmst00() {
+        let theta = erst::gmst00(2400000.5, 53736.0, 2400000.5, 53736.0);
 
-#[test]
-fn test_gmst06() {
-    let theta = gmst06(2400000.5, 53736.0, 2400000.5, 53736.0);
+        vvd(theta, 1.754174972210740592, 1e-12, "gmst00", "status");
+    }
 
-    assert!((theta - 1.754174971870091203).abs() < 1e-12, "gmst06");
-}
+    #[test]
+    fn test_gmst06() {
+        let theta = erst::gmst06(2400000.5, 53736.0, 2400000.5, 53736.0);
 
-#[test]
-fn test_gmst82() {
-    let theta = gmst82(2400000.5, 53736.0);
+        vvd(theta, 1.754174971870091203, 1e-12, "gmst06", "status");
+    }
 
-    assert!((theta - 1.754174981860675096).abs() < 1e-12, "gmst82");
-}
+    #[test]
+    fn test_gmst82() {
+        let theta = erst::gmst82(2400000.5, 53736.0);
 
-#[test]
-fn test_gst00a() {
-    let theta = gst00a(2400000.5, 53736.0, 2400000.5, 53736.0);
+        vvd(theta, 1.754174981860675096, 1e-12, "gmst82", "status");
+    }
 
-    assert!((theta - 1.754166138018281369).abs() < 1e-12, "gst00a");
-}
+    #[test]
+    fn test_gst00a() {
+        let theta = erst::gst00a(2400000.5, 53736.0, 2400000.5, 53736.0);
 
-#[test]
-fn test_gst00b() {
-    let theta = gst00b(2400000.5, 53736.0);
+        vvd(theta, 1.754166138018281369, 1e-12, "gst00a", "status");
+    }
 
-    assert!((theta - 1.754166136510680589).abs() < 1e-12, "gst00b");
-}
+    #[test]
+    fn test_gst00b() {
+        let theta = erst::gst00b(2400000.5, 53736.0);
 
-#[test]
-fn test_gst06() {
-    let rnpb = [
-        [
-            0.9999989440476103608,
-            -0.1332881761240011518e-2,
-            -0.5790767434730085097e-3,
-        ],
-        [
-            0.1332858254308954453e-2,
-            0.9999991109044505944,
-            -0.4097782710401555759e-4,
-        ],
-        [
-            0.5791308472168153320e-3,
-            0.4020595661593994396e-4,
-            0.9999998314954572365,
-        ],
-    ];
+        vvd(theta, 1.754166136510680589, 1e-12, "gst00b", "status");
+    }
 
-    let theta = gst06(2400000.5, 53736.0, 2400000.5, 53736.0, &rnpb);
+    #[test]
+    fn test_gst06() {
+        let rnpb = [
+            [
+                0.9999989440476103608,
+                -0.1332881761240011518e-2,
+                -0.5790767434730085097e-3,
+            ],
+            [
+                0.1332858254308954453e-2,
+                0.9999991109044505944,
+                -0.4097782710401555759e-4,
+            ],
+            [
+                0.5791308472168153320e-3,
+                0.4020595661593994396e-4,
+                0.9999998314954572365,
+            ],
+        ];
 
-    assert!((theta - 1.754166138018167568).abs() < 1e-12, "gst06");
-}
+        let theta = erst::gst06(2400000.5, 53736.0, 2400000.5, 53736.0, &rnpb);
 
-#[test]
-fn test_gst06a() {
-    let theta = gst06a(2400000.5, 53736.0, 2400000.5, 53736.0);
+        vvd(theta, 1.754166138018167568, 1e-12, "gst06", "status");
+    }
 
-    assert!((theta - 1.754166137675019159).abs() < 1e-12, "gst06a");
-}
+    #[test]
+    fn test_gst06a() {
+        let theta = erst::gst06a(2400000.5, 53736.0, 2400000.5, 53736.0);
 
-#[test]
-fn test_gst94() {
-    let theta = gst94(2400000.5, 53736.0);
+        vvd(theta, 1.754166137675019159, 1e-12, "gst06a", "status");
+    }
 
-    assert!((theta - 1.754166136020645203).abs() < 1e-12, "gst94");
+    #[test]
+    fn test_gst94() {
+        let theta = erst::gst94(2400000.5, 53736.0);
+
+        vvd(theta, 1.754166136020645203, 1e-12, "gst94", "status");
+    }
 }
