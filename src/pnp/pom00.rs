ba74536c4ecf0fe166ad118f1c34e3d007771d34
Huxulm <huxulm@gmail.com>
2026-02-26 18:34:05 +0800

    refactor(pnp): update pom00 API to return arrays instead of using mutable reference parameters

--- src/pnp/pom00.rs
+++ src/pnp/pom00.rs
@@ -1,8 +1,10 @@
 use crate::vm::{ir, rx, ry, rz};
 
-pub fn pom00(xp: f64, yp: f64, sp: f64, rpom: &mut [[f64; 3]; 3]) {
-    ir(rpom);
-    rz(sp, rpom);
-    ry(-xp, rpom);
-    rx(-yp, rpom);
+pub fn pom00(xp: f64, yp: f64, sp: f64) -> [[f64; 3]; 3] {
+    let mut rpom = [[0.0; 3]; 3];
+    ir(&mut rpom);
+    rz(sp, &mut rpom);
+    ry(-xp, &mut rpom);
+    rx(-yp, &mut rpom);
+    rpom
 }
