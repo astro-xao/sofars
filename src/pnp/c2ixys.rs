366b883268ece3e4b3284c35fb375d934a12bd20
Huxulm <huxulm@gmail.com>
2026-02-26 18:34:05 +0800

    refactor(pnp): update c2ixys API to return arrays instead of using mutable reference parameters

--- src/pnp/c2ixys.rs
+++ src/pnp/c2ixys.rs
@@ -1,13 +1,15 @@
 use crate::vm::{ir, ry, rz};
 
 ///  Celestial−to−intermediate matrix, given X,Y, IAU 2000
-pub fn c2ixys(x: f64, y: f64, s: f64, rc2i: &mut [[f64; 3]; 3]) {
+pub fn c2ixys(x: f64, y: f64, s: f64) -> [[f64; 3]; 3] {
+    let mut rc2i = [[0.0; 3]; 3];
     let r2 = x * x + y * y;
     let e = if r2 > 0.0 { y.atan2(x) } else { 0.0 };
     let d = (r2 / (1.0 - r2)).sqrt().atan();
 
-    ir(rc2i);
-    rz(e, rc2i);
-    ry(d, rc2i);
-    rz(-(e + s), rc2i);
+    ir(&mut rc2i);
+    rz(e, &mut rc2i);
+    ry(d, &mut rc2i);
+    rz(-(e + s), &mut rc2i);
+    rc2i
 }
