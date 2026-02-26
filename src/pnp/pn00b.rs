bd9bb9ffdc2798cd5152b7122afa10fc39e3a9b3
Huxulm <huxulm@gmail.com>
2026-02-26 18:34:06 +0800

    feat(pnp): implement pn00b routine

---
+++ src/pnp/pn00b.rs
@@ -0,0 +1,120 @@
+use super::{nut00b, pn00};
+
+///  Bias/precession/nutation, IAU 2000B
+///
+///  Precession-nutation, IAU 2000B model:  a multi-purpose function,
+///  supporting classical (equinox-based) use directly and CIO-based
+///  use indirectly.
+///
+///  This function is part of the International Astronomical Union's
+///  SOFA (Standards of Fundamental Astronomy) software collection.
+///
+///  Status:  support function.
+///
+///  Given:
+///     date1,date2  double          TT as a 2-part Julian Date (Note 1)
+///
+///  Returned (function value):
+///     (dpsi, deps, epsa, rb, rp, rbp, rn, rbpn) (f64, f64, f64, [[f64; 3]; 3], [[f64; 3]; 3], [[f64; 3]; 3], [[f64; 3]; 3], [[f64; 3]; 3])
+///
+///     dpsi,deps    nutation (Note 2)
+///     epsa         mean obliquity (Note 3)
+///     rb           frame bias matrix (Note 4)
+///     rp           precession matrix (Note 5)
+///     rbp          bias-precession matrix (Note 6)
+///     rn           nutation matrix (Note 7)
+///     rbpn         GCRS-to-true matrix (Notes 8,9)
+///
+///  Notes:
+///
+///  1)  The TT date date1+date2 is a Julian Date, apportioned in any
+///      convenient way between the two arguments.  For example,
+///      JD(TT)=2450123.7 could be expressed in any of these ways,
+///      among others:
+///
+///             date1          date2
+///
+///          2450123.7           0.0       (JD method)
+///          2451545.0       -1421.3       (J2000 method)
+///          2400000.5       50123.2       (MJD method)
+///          2450123.5           0.2       (date & time method)
+///
+///      The JD method is the most natural and convenient to use in
+///      cases where the loss of several decimal digits of resolution
+///      is acceptable.  The J2000 method is best matched to the way
+///      the argument is handled internally and will deliver the
+///      optimum resolution.  The MJD method and the date & time methods
+///      are both good compromises between resolution and convenience.
+///
+///  2)  The nutation components (luni-solar + planetary, IAU 2000B) in
+///      longitude and obliquity are in radians and with respect to the
+///      equinox and ecliptic of date.  For the utmost accuracy, use the
+///      iauPn00 function, where the nutation components are caller-
+///      specified.
+///
+///  3)  The mean obliquity is consistent with the IAU 2000 precession.
+///
+///  4)  The matrix rb transforms vectors from GCRS to J2000.0 mean
+///      equator and equinox by applying frame bias.
+///
+///  5)  The matrix rp transforms vectors from J2000.0 mean equator and
+///      equinox to mean equator and equinox of date by applying
+///      precession.
+///
+///  6)  The matrix rbp transforms vectors from GCRS to mean equator and
+///      equinox of date by applying frame bias then precession.  It is
+///      the product rp x rb.
+///
+///  7)  The matrix rn transforms vectors from mean equator and equinox
+///      of date to true equator and equinox of date by applying the
+///      nutation (luni-solar + planetary).
+///
+///  8)  The matrix rbppn transforms vectors from GCRS to true equator and
+///      equinox of date.  It is the product rn x rbp, applying frame
+///      bias, precession and nutation in that order.
+///
+///  9)  The X,Y,Z coordinates of the IAU 2000B Celestial Intermediate
+///      Pole are elements (3,1-3) of the GCRS-to-true matrix,
+///      i.e. rbpn[2][0-2].
+///
+///  Called:
+///     iauNut00b    nutation, IAU 2000B
+///     iauPn00      bias/precession/nutation results, IAU 2000
+///
+///  Reference:
+///
+///     Capitaine, N., Chapront, J., Lambert, S. and Wallace, P.,
+///     "Expressions for the Celestial Intermediate Pole and Celestial
+///     Ephemeris Origin consistent with the IAU 2000A precession-
+///     nutation model", Astron.Astrophys. 400, 1145-1154 (2003)
+///
+///     n.b. The celestial ephemeris origin (CEO) was renamed "celestial
+///          intermediate origin" (CIO) by IAU 2006 Resolution 2.
+///
+///  This revision:  2021 May 11
+///
+///  SOFA release 2023-10-11
+///
+///  Copyright (C) 2023 IAU SOFA Board.  See notes at end.
+///
+pub fn pn00b(
+    date1: f64,
+    date2: f64,
+) -> (
+    f64,
+    f64,
+    f64,
+    [[f64; 3]; 3],
+    [[f64; 3]; 3],
+    [[f64; 3]; 3],
+    [[f64; 3]; 3],
+    [[f64; 3]; 3],
+) {
+    /* Nutation. */
+    let (dpsi, deps) = nut00b(date1, date2);
+
+    /* Remaining results. */
+    let (epsa, rb, rp, rbp, rn, rbpn) = pn00(date1, date2, dpsi, deps);
+
+    (dpsi, deps, epsa, rb, rp, rbp, rn, rbpn)
+}
