a67afc9af0439f68ea9c27980515982bd4ee81d6
Huxulm <huxulm@gmail.com>
2026-02-26 18:34:05 +0800

    refactor(pnp): update fw2m API to return arrays instead of using mutable reference parameters

--- src/pnp/fw2m.rs
+++ src/pnp/fw2m.rs
@@ -1,10 +1,12 @@
 use crate::vm::{ir, rx, rz};
 
-pub fn fw2m(gamb: f64, phib: f64, psi: f64, eps: f64, r: &mut [[f64; 3]; 3]) {
+pub fn fw2m(gamb: f64, phib: f64, psi: f64, eps: f64) -> [[f64; 3]; 3] {
+    let mut r = [[0.0; 3]; 3];
     /* Construct the matrix. */
-    ir(r);
-    rz(gamb, r);
-    rx(phib, r);
-    rz(-psi, r);
-    rx(-eps, r);
+    ir(&mut r);
+    rz(gamb, &mut r);
+    rx(phib, &mut r);
+    rz(-psi, &mut r);
+    rx(-eps, &mut r);
+    r
 }
