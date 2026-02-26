78b119075bed35d03e26049eeab4a7bd67ba09d2
Huxulm <huxulm@gmail.com>
2026-02-26 18:34:05 +0800

    feat(pnp): register newly added pnp module routines

--- src/pnp/mod.rs
+++ src/pnp/mod.rs
@@ -5,9 +5,6 @@ pub use bi00::*;
 mod bp00;
 pub use bp00::*;
 
-mod pnp;
-pub use pnp::*;
-
 mod pn00;
 pub use pn00::*;
 
@@ -53,6 +50,18 @@ pub use pr00::*;
 mod s06;
 pub use s06::*;
 
+mod s06a;
+pub use s06a::*;
+
+mod s00;
+pub use s00::*;
+
+mod s00a;
+pub use s00a::*;
+
+mod s00b;
+pub use s00b::*;
+
 mod sp00;
 pub use sp00::*;
 
@@ -67,3 +76,113 @@ pub use eors::*;
 
 mod obl06;
 pub use obl06::*;
+
+mod pmat00;
+pub use pmat00::*;
+
+mod pmat06;
+pub use pmat06::*;
+
+mod pnm00b;
+pub use pnm00b::*;
+
+mod pn00b;
+pub use pn00b::*;
+
+mod bp06;
+pub use bp06::*;
+
+mod c2i00a;
+pub use c2i00a::*;
+
+mod c2i00b;
+pub use c2i00b::*;
+
+mod c2i06a;
+pub use c2i06a::*;
+
+mod c2ibpn;
+pub use c2ibpn::*;
+
+mod c2ixy;
+pub use c2ixy::*;
+
+mod c2t00a;
+pub use c2t00a::*;
+
+mod c2t00b;
+pub use c2t00b::*;
+
+mod c2t06a;
+pub use c2t06a::*;
+
+mod c2tcio;
+pub use c2tcio::*;
+
+mod c2teqx;
+pub use c2teqx::*;
+
+mod c2tpe;
+pub use c2tpe::*;
+
+mod c2txy;
+pub use c2txy::*;
+
+mod eo06a;
+pub use eo06a::*;
+
+mod fw2xy;
+pub use fw2xy::*;
+
+mod ltp;
+pub use ltp::*;
+
+mod ltpb;
+pub use ltpb::*;
+
+mod ltpecl;
+pub use ltpecl::*;
+
+mod ltpequ;
+pub use ltpequ::*;
+
+mod num00a;
+pub use num00a::*;
+
+mod num00b;
+pub use num00b::*;
+
+mod num06a;
+pub use num06a::*;
+
+mod nutm80;
+pub use nutm80::*;
+
+mod pb06;
+pub use pb06::*;
+
+mod pn06;
+pub use pn06::*;
+
+mod pn06a;
+pub use pn06a::*;
+
+mod pmat76;
+pub use pmat76::*;
+
+mod prec76;
+pub use prec76::*;
+
+mod pnm80;
+pub use pnm80::*;
+
+mod p06e;
+pub use p06e::*;
+mod xy06;
+pub use xy06::*;
+mod xys00a;
+pub use xys00a::*;
+mod xys00b;
+pub use xys00b::*;
+mod xys06a;
+pub use xys06a::*;
